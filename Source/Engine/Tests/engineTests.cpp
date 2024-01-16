7e8f5bd31192868e740358dd661287468902bffb
Rahul Vadhyar <vadhyarrahul@gmail.com>
2024-01-16 15:47:19 +0530

    starting to create unit tests

---
+++ Source/Engine/Tests/engineTests.cpp
@@ -0,0 +1,47 @@
+#include "gtest/gtest.h"
+#include "helper.hpp"
+
+TEST(isWorking, EngineTest){
+
+}
+TEST(multipleDrawFramePerLoop EngineTest){
+
+}
+
+TEST(changeWindowWidth EngineTest){
+
+}
+TEST(changeWindowHeight EngineTest){
+
+}
+TEST(changeWindowWidthAndHeight EngineTest){
+
+}
+TEST(setWindowWidthZero EngineTest){
+
+}
+TEST(setWindowHeightZero EngineTest){
+
+}
+TEST(setWindowWidthAndHeightZero EngineTest){
+
+}
+
+TEST(setWindowWidthNegative EngineTest){
+
+}
+TEST(setWindowHeightNegative EngineTest){
+
+}
+TEST(setWindowWidthAndHeightNegative EngineTest){
+
+}
+TEST(changeBGColor EngineTest){
+
+}
+TEST(changeBGColorNegative EngineTest){
+
+}
+TEST(BGColorOutOfBounds EngineTest){
+
+}
---
+++ Source/Engine/Tests/textManagerTests.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "helper.hpp"
+
+TEST(isWorking, TextTest){
+
+}
+TEST(){
+
+}
