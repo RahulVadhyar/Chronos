8e5037eb9a5a25c1b0704debbbb3db5286896435
Rahul Vadhyar <vadhyarrahul@gmail.com>
2024-03-13 17:45:34 +0530

    added cmake options to allow selective builds. Editor is now its own executable

---
+++ Source/Editor/Source/editorMain.cpp
@@ -0,0 +1,24 @@
+#include "chronos.hpp"
+
+int main(){
+
+    std::filesystem::path cwdpath("../");
+    std::filesystem::current_path(cwdpath);
+    Chronos::Manager::Initializer initializer;
+    initializer.WindowWidth = 800;
+    initializer.WindowHeight = 600;
+    initializer.BackgroundColor[0] = 0;
+    initializer.BackgroundColor[1] = 0;
+    initializer.BackgroundColor[2] = 0;
+
+    Chronos::Manager::Manager manager(initializer);
+    
+    while (!glfwWindowShouldClose(manager.getWindow())) {
+        if (glfwGetKey(manager.getWindow(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(manager.getWindow(), true);
+        }
+        manager.drawFrame();
+    }
+    
+    return 0;
+}
