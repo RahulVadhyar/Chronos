00bcfa2f0955e31ea809429af35299c2c6fc3276
Rahul Vadhyar <vadhyarrahul@gmail.com>
2024-03-27 03:56:04 +0530

    update shaders and compile code for windows

--- Shaders/Vertex/textureShader.vert
+++ Shaders/Vertex/textureShader.vert
@@ -1,5 +1,5 @@
 #version 450
-
+#extension GL_EXT_debug_printf : enable
 layout(binding = 0) uniform UniformBufferObject {
     mat4 model;
     mat4 view;
@@ -12,6 +12,7 @@ layout(location = 1) in vec2 inTexCoord;
 layout(location = 0) out vec2 fragTexCoord;
 
 void main() {
+    debugPrintfEXT("inPosition is %f and %f", inPosition.x, inPosition.y);
     gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 0.0, 1.0);
     fragTexCoord = inTexCoord;
 }
--- Shaders/compile.bat
+++ Shaders/compile.bat
@@ -1,7 +1,11 @@
-"C:\VulkanSDK\1.3.261.1\Bin\glslc.exe" shader.vert -o vert.spv
-"C:\VulkanSDK\1.3.261.1\Bin\glslc.exe" shader.frag -o frag.spv
-"C:\VulkanSDK\1.3.261.1\Bin\glslc.exe" circle.vert -o circle_vert.spv
-"C:\VulkanSDK\1.3.261.1\Bin\glslc.exe" circle.frag -o circle_frag.spv
-"C:\VulkanSDK\1.3.261.1\Bin\glslc.exe" text.vert -o textVert.spv
-"C:\VulkanSDK\1.3.261.1\Bin\glslc.exe" text.frag -o textFrag.spv
+glslc Vertex/shader.vert -o SPIV/vert.spv
+glslc Fragment/shader.frag -o SPIV/frag.spv
+glslc Vertex/colorShader.vert -o SPIV/colorVert.spv
+glslc Fragment/colorShader.frag -o SPIV/colorFrag.spv
+glslc Vertex/textureShader.vert -o SPIV/textureVert.spv
+glslc Fragment/textureShader.frag -o SPIV/textureFrag.spv
+glslc Vertex/circle.vert -o SPIV/circle_vert.spv
+glslc Fragment/circle.frag -o SPIV/circle_frag.spv
+glslc Vertex/text.vert -o SPIV/textVert.spv
+glslc Fragment/text.frag -o SPIV/textFrag.spv
 pause
